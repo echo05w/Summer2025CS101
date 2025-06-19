5d72c46aeb7d09a85d608ae9f89916aee8940bc6
h.soliyev <h.soliyev@newuu.uz>
2025-06-19 16:17:14 +0500

    bankay

--- src/week3/lab3/problem4.cpp
+++ src/week3/lab3/problem4.cpp
@@ -3,7 +3,15 @@
 //
 #include <iostream>
 using namespace std;
-int main() {
+int sum_Of_digits(int num) {
+    if(num==0) return 0;
+     return num%10+sum_Of_digits(num/10);
+}
 
+int main() {
+int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    cout<<sum_Of_digits(num)<<endl;
     return 0;
 }
--- src/week3/lab3/problem5.cpp
+++ src/week3/lab3/problem5.cpp
@@ -3,7 +3,17 @@
 //
 #include <iostream>
 using namespace std;
-int main() {
+int multiplication(int num1, int num2) {
+    if(num2==0){ return 0;}
+    if (num2>0){return num1+multiplication(num1,num2-1);}
+    else  {return -num1+multiplication(num1,num2-1) ;}
 
+}
+
+int main() {
+    int a , b;
+    cout<<"Enter a numbers: ";
+    cin>>a>>b;
+    cout<<multiplication(a,b)<<endl;
     return 0;
 }
