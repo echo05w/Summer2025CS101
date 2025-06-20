2b41d4680b692ad5da1b3b3b6b89354cb669d9ad
h.soliyev <h.soliyev@newuu.uz>
2025-06-20 17:04:42 +0500

    huh

--- src/week3/lab4/problem6.cpp
+++ src/week3/lab4/problem6.cpp
@@ -1,17 +1,39 @@
-//
-// Created by HASAN SOLIYEV on 20/06/25.
-//
 #include <iostream>
 using namespace std;
+
+void character_types(char arr[], int n, int &up, int &low, int &d, int &s) {
+    up = low = d = s = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= 65 && arr[i] <= 90) {
+            up++;
+        } else if (arr[i] >= 97 && arr[i] <= 122) {
+            low++;
+        } else if (arr[i] >= 48 && arr[i] <= 57) {
+            d++;
+        } else {
+            s++;
+        }
+    }
+}
 
 int main() {
     int n;
     cout << "Please enter a number: ";
     cin >> n;
-    int arr[n];
+
+    char arr[n];
+    cout << "Enter " << n << " characters (no spaces): ";
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    if ("A"="Z")
+
+    int amountU, amountL, amountD, amountS;
+    character_types(arr, n, amountU, amountL, amountD, amountS);
+
+    cout << "Amount of U: " << amountU << endl;
+    cout << "Amount of L: " << amountL << endl;
+    cout << "Amount of D: " << amountD << endl;
+    cout << "Amount of S: " << amountS << endl;
+
     return 0;
 }
--- src/week3/lab4/problem7.cpp
+++ src/week3/lab4/problem7.cpp
@@ -2,8 +2,16 @@
 // Created by HASAN SOLIYEV on 20/06/25.
 //
 #include <iostream>
+#include <ctime>
 using namespace std;
 int main() {
-
+ srand(time(0));
+    int arr[6]={};
+    for (int i = 0; i < 10000; i++) {
+       arr[1+rand()%6-1]++;
+    }
+for (int i = 0; i <6; i++) {
+    cout << i+1<<":"<<arr[i] << endl;
+}
     return 0;
 }
