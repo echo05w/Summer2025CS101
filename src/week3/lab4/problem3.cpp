ae4626aa1400eb9b1bae60e9d3e163c651c10ee9
h.soliyev <h.soliyev@newuu.uz>
2025-06-20 15:51:30 +0500

    huh

--- src/week3/lab4/problem3.cpp
+++ src/week3/lab4/problem3.cpp
@@ -3,7 +3,26 @@
 //
 #include <iostream>
 using namespace std;
-int main() {
 
-    return 0;
+int main() {
+    int n;
+   cout << "Please enter a number: ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int min = arr[0];
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    for (int i=1;i<n;i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    cout <<"Min is: "<< min << " Max is: " << max << endl;
 }
