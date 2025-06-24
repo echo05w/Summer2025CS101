9caaad7a904d8a67992ba19d71689840e0d321a1
h.soliyev <h.soliyev@newuu.uz>
2025-06-24 16:12:48 +0500

    lab1 pr6 and pr7 (Hello professor i just renamed folder from week5 to week 4 other stuff hasnt been updated)

--- src/week4/lab2/problem1.cpp
+++ src/week4/lab2/problem1.cpp
@@ -3,7 +3,13 @@
 //
 #include <iostream>
 using namespace std;
-int main() {
+int main(){
+    int a[10]={1,2,3,4,5,6,7,8,9,10};
+  int *ptr=a;
+    for (int i = 0; i < 10; i++) {
+        cout << *ptr << " "<<ptr<<endl;
+        ptr++;
+    }
 
     return 0;
 }
