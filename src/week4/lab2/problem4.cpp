7913976587211777782292f8545a1e0b4fd1ecb9
h.soliyev <h.soliyev@newuu.uz>
2025-06-24 16:53:15 +0500

    lab1 pr6 and pr7 (Hello professor i just renamed folder from week5 to week 4 other stuff hasnt been updated)

--- src/week4/lab2/problem4.cpp
+++ src/week4/lab2/problem4.cpp
@@ -3,7 +3,27 @@
 //
 #include <iostream>
 using namespace std;
+bool is_mirrored(const int* a, const int* b,int size) {
+    for (int i = 0; i < size; i++) {
+        if (a[i] != b[size-i-1]) {
+            return false;
+        }
+    }
+    return true;
+}
 int main() {
-
+    int n;
+    cout<<"Enter n: "<<endl;
+    cin >> n;
+int a[n];
+    int b[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    int result=is_mirrored(a,b,n);
+    cout<<"If 0 false , otherwise "<<endl<<result<<endl;
     return 0;
 }
--- src/week4/lab2/problem5.cpp
+++ src/week4/lab2/problem5.cpp
@@ -1,9 +1,36 @@
-//
-// Created by HASAN SOLIYEV on 24/06/25.
-//
 #include <iostream>
 using namespace std;
+
+void sumArrays(const double* arr1, const double* arr2, double* sum, int size) {
+    for (int i = 0; i < size; i++) {
+        sum[i] = arr1[i] + arr2[i];
+    }
+}
+
 int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    double arr1[n], arr2[n], sum[n];
+
+    cout << "Enter " << n << " values for array 1:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+
+    cout << "Enter " << n << " values for array 2:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr2[i];
+    }
+
+    sumArrays(arr1, arr2, sum, n);
+
+    cout << "Summed array:\n";
+    for (int i = 0; i < n; i++) {
+        cout << sum[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
