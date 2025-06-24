5cbad87ca524b16b0c9ba83dcf7d3d373cc6af9f
h.soliyev <h.soliyev@newuu.uz>
2025-06-24 16:35:03 +0500

    lab1 pr6 and pr7 (Hello professor i just renamed folder from week5 to week 4 other stuff hasnt been updated)

--- src/week4/lab2/problem2.cpp
+++ src/week4/lab2/problem2.cpp
@@ -1,9 +1,24 @@
 //
 // Created by HASAN SOLIYEV on 24/06/25.
 //
+
 #include <iostream>
 using namespace std;
-int main() {
+int main(){
+    float a[10]={};
+    cout<<"Enter grades: "<<endl;
+    float *ptr=a;
+    for (int i = 0; i < 10; i++) {
+        cin>>*ptr;
+        *ptr = *ptr*0.3;
+        ptr++;
+    }
+    ptr=a;
+    for (int i = 0; i < 10; i++) {
+        cout << *ptr << " ";
+        ptr++;
+    }
+
 
     return 0;
 }
--- src/week4/lab2/problem3.cpp
+++ src/week4/lab2/problem3.cpp
@@ -1,9 +1,37 @@
 //
 // Created by HASAN SOLIYEV on 24/06/25.
 //
+
 #include <iostream>
 using namespace std;
-int main() {
-
+int evenCount(const int* a, int size) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (a[i] % 2 == 0) {
+            count += 1;
+        }
+    }
+    return count;
+}
+int oddCount(const int* a, int size) {
+    int countOdd = 0;
+    for (int i = 0; i < size; i++) {
+        if (a[i] % 2 != 0) {
+            countOdd += 1;
+        }
+    }
+    return countOdd;
+}
+int main(){
+int n;
+    cout<<"Enter n: "<<endl;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int result = evenCount(arr, n);
+    int result2 = oddCount(arr, n);
+    cout << "THe amount of even is "<<result<<endl<<"The amount of odd is "<< result2<< endl;
     return 0;
 }
