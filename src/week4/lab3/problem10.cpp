ec225be6dacdd3057f694c451afcf4a4ba670b86
h.soliyev <h.soliyev@newuu.uz>
2025-06-26 16:39:18 +0500

    спасибо

--- src/week4/lab3/problem10.cpp
+++ src/week4/lab3/problem10.cpp
@@ -1,3 +1,34 @@
 //
 // Created by HASAN SOLIYEV on 26/06/25.
 //
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int* arr = new int[n];
+
+    cout << "Enter " << n << " integers:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> *(arr + i);
+    }
+    int oddCount = 0;
+    int evenCount = 0;
+    int* p = arr;
+    for (int i = 0; i < n; i++) {
+        if (*(p + i) % 2 != 0) {
+            oddCount++;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (*(arr + i) % 2 == 0) {
+            evenCount++;
+        }
+    }
+    cout << "Number of odd elements: " << oddCount << endl;
+    cout << "Number of even elements: " << evenCount << endl;
+    delete[] arr;
+    return 0;
+}
--- src/week4/lab3/problem9.cpp
+++ src/week4/lab3/problem9.cpp
@@ -1,3 +1,22 @@
 //
 // Created by HASAN SOLIYEV on 26/06/25.
 //
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    double* arr = new double[n];
+    cout << "Enter " << n << " double values:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> *(arr + i);
+    }
+    cout << "Values from end to front:\n";
+    for (int i = n - 1; i >= 0; i--) {
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
+    delete[] arr;
+    return 0;
+}
