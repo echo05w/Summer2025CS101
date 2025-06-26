5b70cc6bc682e748701fa6fc4f9324507ebffdb7
h.soliyev <h.soliyev@newuu.uz>
2025-06-26 16:32:24 +0500

    спасибо

--- src/week4/lab3/problem5.cpp
+++ src/week4/lab3/problem5.cpp
@@ -2,4 +2,26 @@
 // Created by HASAN SOLIYEV on 26/06/25.
 //
 #include <iostream>
+#include <ctime>
 using namespace std;
+void printMatrix(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int num=rand()%1001;
+            cout<<num<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main() {
+    int n;
+    cout<<"Enter number of rows: ";
+    cin>>n;
+    srand(time(NULL));
+    cout<<n<<"x"<<n<<endl;
+    printMatrix(n);
+
+    cout<<"Your matrix is "<<n<<" by "<<n<<endl;
+
+    return 0;
+}
--- src/week4/lab3/problem8.cpp
+++ src/week4/lab3/problem8.cpp
@@ -1,3 +1,18 @@
 //
 // Created by HASAN SOLIYEV on 26/06/25.
 //
+#include <iostream>
+using namespace std;
+void swapAandB(int &a, int &b) {
+    a=a+b;
+    b=a-b;
+    a=a-b;
+}
+int main() {
+   int a,b;
+    cout<<"Enter a and b : ";
+    cin>>a>>b;
+   swapAandB(a,b);
+    cout<<"After swapping a: "<<a<<" b: "<<b<<endl;
+    return 0;
+}
